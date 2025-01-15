a539f70769f2fb577d0e1511edbe4e5e5713d637
BlessingOkora <moraablessing082@gmail.com>
2025-01-15 12:45:24 +0300

    sends get requests

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,20 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
-
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status() 
+            return response.content
+        except requests.RequestException as e:
+            print(f"An error occurred: {e}")
+            return None
+        
     def load_json(self):
-        pass
+        try:
+            response_body = self.get_response_body()
+            if response_body is not None:
+                return json.loads(response_body)
+            return None
+        except json.JSONDecodeError as e:
+            print(f"Failed to decode JSON: {e}")
+            return None
